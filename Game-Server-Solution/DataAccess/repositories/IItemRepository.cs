65f91bd30edcaf1175c19cf087ba7bb2dc261850
Jonathan <jdela136@gmail.com>
2021-03-16 18:56:19 -0400

    added Item Repository and controller

---
+++ Game-Server-Solution/DataAccess/repositories/IItemRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.repositories
+{
+    public interface IItemRepository
+    {
+        IEnumerable<Weapon> GetAllWeapons();
+
+        Weapon GetWeapon(int id);
+
+        IEnumerable<Consumable> GetAllConsumables();
+
+        Consumable GetConsumable(int id);
+
+        Consumable GetRandomConsumable();
+
+
+ 
+    }
+}
---
+++ Game-Server-Solution/GameAPI/Controllers/ItemController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.repositories;
+using DataAccess;
+
+namespace GameAPI.Controllers
+{
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly IItemRepository _itemRepository;
+
+        public ItemController(IItemRepository itemRepository)
+        {
+            _itemRepository = itemRepository;
+        }
+
+        [HttpGet("/api/weapons")]
+        public IEnumerable<Weapon> GetAllWeapons()
+        {
+            return _itemRepository.GetAllWeapons();
+        }
+
+        [HttpGet("/api/weapons/{id}")]
+        public Weapon GetWeapon(int id)
+        {
+            return _itemRepository.GetWeapon(id);
+        }
+
+        [HttpGet("/api/consumables")]
+        IEnumerable<Consumable> GetAllConsumables()
+        {
+            return _itemRepository.GetAllConsumables();
+        }
+
+        [HttpGet("/api/consumables/{id}")]
+        public Consumable GetConsumable(int id)
+        {
+            return _itemRepository.GetConsumable(id);
+        }
+
+        [HttpGet("/api/consumables/random")]
+        public Consumable GetRandomConsumable()
+        {
+            return _itemRepository.GetRandomConsumable();
+        }
+    }
+}
