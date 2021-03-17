c8a892cacbe2ae9bddad3d55064dd013f3b605ce
Hamza Butt <ibnetahir1@gmail.com>
2021-03-17 16:06:08 -0400

    Commented out a failing test in ConsumableServiceTest - this was a bad test.

--- Game-Server-Solution/UnitTests/ConsumableServiceTests.cs
+++ Game-Server-Solution/UnitTests/ConsumableServiceTests.cs
@@ -14,33 +14,33 @@ namespace UnitTests
             var consumableService = new ConsumableService(null);
         }
 
-        [Fact]
-        public void GetRandomConsumable_GetsOneRandomConsumable()
-        {
-            // arrange
-            var consumables = new[]
-            {
-                new Consumable {Id = 1, ItemName = "Strength Potion", Description = "Gives Strength points" , Duration = 60,
-                Strength = 1.2, Type = "Strength"},
-                new Consumable {Id = 2, ItemName = "Health Potion", Description = "Gives Health points" , Duration = 60,
-                Strength = 5, Type = "Health"}
-            };
+        //[Fact]
+        //public void GetRandomConsumable_GetsOneRandomConsumable()
+        //{
+        //    // arrange
+        //    var consumables = new[]
+        //    {
+        //        new Consumable {Id = 1, ItemName = "Strength Potion", Description = "Gives Strength points" , Duration = 60,
+        //        Strength = 1.2, Type = "Strength"},
+        //        new Consumable {Id = 2, ItemName = "Health Potion", Description = "Gives Health points" , Duration = 60,
+        //        Strength = 5, Type = "Health"}
+        //    };
 
-            var mockRepo = new Mock<IItemRepository>();
-            mockRepo.Setup(r => r.GetAllConsumables()).Returns(consumables).Verifiable();
-            mockRepo.Setup(r => r.GetConsumable(It.IsAny<int>())).Verifiable();
+        //    var mockRepo = new Mock<IItemRepository>();
+        //    mockRepo.Setup(r => r.GetAllConsumables()).Returns(consumables).Verifiable();
+        //    mockRepo.Setup(r => r.GetConsumable(It.IsAny<int>())).Verifiable();
 
-            var consumableService = new ConsumableService(mockRepo.Object);
+        //    var consumableService = new ConsumableService(mockRepo.Object);
 
-            // act
-            consumableService.GetRandomConsumable();
+        //    // act
+        //    consumableService.GetRandomConsumable();
 
-            // assert
-            Random gen = new Random();
-            int randomId = gen.Next(consumables.Length);
-            mockRepo.Verify(r => r.GetAllConsumables());
-            mockRepo.Verify(r => r.GetConsumable(randomId));
-            mockRepo.VerifyNoOtherCalls();
-        }
+        //    // assert
+        //    Random gen = new Random();
+        //    int randomId = gen.Next(consumables.Length);
+        //    mockRepo.Verify(r => r.GetAllConsumables());
+        //    mockRepo.Verify(r => r.GetConsumable(randomId));
+        //    mockRepo.VerifyNoOtherCalls();
+        //}
     }
 }
